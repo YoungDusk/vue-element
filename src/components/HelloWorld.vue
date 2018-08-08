3e9208c5bd824bb06175bac6620789bbb659088a
LiaoWeiLin <2476127622@qq.com>
2018-08-09 06:43:29 +0800

    element

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="hello">
+    <el-button plain @click="open3">成功</el-button>
+    <el-button plain @click="open4">警告</el-button>
+    <el-button plain @click="open5">消息</el-button>
+    <el-button plain @click="open6">错误</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      
+    }
+  },
+  methods: {
+    open3() {
+      this.$notify({
+        title: '成功',
+        message: '这是一条成功的提示消息',
+        type: 'success'
+      });
+    },
+
+    open4() {
+      this.$notify({
+        title: '警告',
+        message: '这是一条警告的提示消息',
+        type: 'warning'
+      });
+    },
+
+    open5() {
+      this.$notify.info({
+        title: '消息',
+        message: '这是一条消息的提示消息'
+      });
+    },
+
+    open6() {
+      this.$notify.error({
+        title: '错误',
+        message: '这是一条错误的提示消息'
+      });
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import { Button, Select, Notification } from 'element-ui';
+
+Vue.use(Button);
+Vue.use(Select);
+Vue.prototype.$notify = Notification;
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
